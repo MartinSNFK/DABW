7eedb425c0cf75e46b78d070efde09c0dfd000ed
MartinSNFK <149702047+MartinSNFK@users.noreply.github.com>
2023-11-02 16:53:12 +0000

    Update streamlit_app.py
    add the pandas frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Omega 3 with blueberry oatmeal')
 streamlit.text('Kale, spinach and rocket Smoothie')
 streamlit.text('Hard boiled free range egg')
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
