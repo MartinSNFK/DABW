06299e44acc9ded3e6a621fb43712cc5ed1f62ab
MartinSNFK <149702047+MartinSNFK@users.noreply.github.com>
2023-11-14 15:56:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+my_cur.execute("insert into fruit_load_list values (from streamlit')")
+
 my_cur.execute("SELECT * from fruit_load_list")
 
 # my_data_row = my_cur.fetchone()
