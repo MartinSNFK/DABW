1dffe19b57758c4820366b19e14cfe583b4076b4
MartinSNFK <149702047+MartinSNFK@users.noreply.github.com>
2023-11-14 11:11:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ streamlit.text('Omega 3 with blueberry oatmeal')
 streamlit.text('Kale, spinach and rocket Smoothie')
 streamlit.text('Hard boiled free range egg')
 
+
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
