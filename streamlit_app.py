e7373434d02d8ece45b015978832b9dbda475169
MartinSNFK <149702047+MartinSNFK@users.noreply.github.com>
2023-11-15 11:25:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,10 @@
 import streamlit
 import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
-#my_cur.execute("insert into fruit_load_list values (from streamlit')")
-
-#my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("insert into fruit_load_list values (from streamlit')")
+my_cur.execute("SELECT * from fruit_load_list")
 streamlit.header('The fruit load list contains:')
-#my_data_rows = my_cur.fetchall()
+my_data_rows = my_cur.fetchall()
 streamlit.dataframe(my_data_rows)
 
 # streamlit.text("The fruit load list contains:")
@@ -45,3 +38,9 @@ streamlit.dataframe(fruityvice_normalized)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
