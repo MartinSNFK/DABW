57b590330deeedb6834ab19ad97c5a65d6efcb5e
MartinSNFK <149702047+MartinSNFK@users.noreply.github.com>
2023-11-15 13:31:56 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,27 +20,24 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-#comment2
-#streamlit.text(fruityvice_response.json())
-
+#new section
 streamlit.header("Fruityvice Fruit Advice")
-
-try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-   if not fruitchoice:
-      streamlit.error("Please select a fruit to get information.")
-   else:
-     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-     streamlit.dataframe(fruityvice_normalized)
+   streamlit.write('The user entered ', fruit_choice)
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-except URLError as e:
-   streamlit.error()
+#try:
+#   if not fruitchoice:
+#      streamlit.error("Please select a fruit to get information.")
+#else:
+#     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+#     streamlit.dataframe(fruityvice_normalized)
 
+#except URLError as e:
+#   streamlit.error()
 
-streamlit.write('The user entered ', fruit_choice)
-
-streamlit.stop()
+#streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
